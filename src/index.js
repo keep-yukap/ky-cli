760fdb050e401760496261871410741e99cbc184
yukap <jingweirong@mistong.com>
2018-09-10 17:54:48 +0800

    feat: add eslint check in pre-commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-<<<<<<< Updated upstream
 import eslint from 'eslint';
 import webpack from 'webpack';
 
 console.log(eslint, webpack);
-=======
-import webpack from 'webpack';
-
-console.log(webpack);
->>>>>>> Stashed changes
